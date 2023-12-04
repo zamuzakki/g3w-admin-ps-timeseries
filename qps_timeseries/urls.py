bb776f76528bc8cc19f76d4ce189dc0e6d765adf
wlorenzetti <lorenzett@gis3w.it>
2023-12-04 12:39:27 +0100

    Add urls module.

---
+++ qps_timeseries/urls.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+"""" Urls module for qps_timeseries
+.. note:: This program is free software; you can redistribute it and/or modify
+    it under the terms of the Mozilla Public License 2.0.
+
+"""
+
+__author__ = 'lorenzetti@gis3w.it'
+__date__ = '2023-12-04'
+__copyright__ = 'Copyright 2015 - 2023, Gis3w'
+__license__ = 'MPL 2.0'
+
+
+urlpatterns = []

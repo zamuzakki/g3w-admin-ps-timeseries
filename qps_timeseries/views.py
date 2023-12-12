ac7b71451d974805103fc43c0c3ac867119ebbba
Raruto <Raruto@users.noreply.github.com>
2023-12-12 15:03:19 +0100

    styling

--- qps_timeseries/views.py
+++ qps_timeseries/views.py
@@ -6,6 +6,8 @@ from django.http.response import JsonResponse
 class QPSTimeseriesPlot(View):
 
     def get(self, *args, **kwargs):
+        DELTA = 5
+        TITLE = 'PS Time Series Viewer<br><sub>coher.: <pid> vel.: <pid> v_stdev.: <pid></sub>'
         ## Fake Data
         ## https://plotly.com/javascript/
         return JsonResponse({
@@ -22,7 +24,47 @@ class QPSTimeseriesPlot(View):
                 'mode': 'markers',
                 'type': 'scatter',
                 'name': 'Scatter',
-                'marker': { 'size': 5, 'color': 'black' }
+                'marker': {
+                  'size': 8,
+                  'color': 'black',
+                  'symbol': 'square',
+                },
+              },
+              {
+                'x': [
+                  '2013-08-04 22:23:00',
+                  '2013-09-04 22:23:00',
+                  '2013-10-04 22:23:00',
+                  '2013-11-04 22:23:00',
+                  '2013-12-04 22:23:00',
+                ],
+                'y': [DELTA + 4, DELTA + 1, DELTA + 7, DELTA + 1, DELTA + 4],
+                'mode': 'scatter',
+                'type': 'scatter',
+                'name': 'Replica +' + str(DELTA),
+                'marker': {
+                  'size': 8,
+                  'color': 'blue',
+                  'symbol': 'square',
+                },
+              },
+              {
+                'x': [
+                  '2013-08-04 22:23:00',
+                  '2013-09-04 22:23:00',
+                  '2013-10-04 22:23:00',
+                  '2013-11-04 22:23:00',
+                  '2013-12-04 22:23:00',
+                ],
+                'y': [4 - DELTA, 1 - DELTA, 7 - DELTA, 1 - DELTA, 4 - DELTA],
+                'mode': 'scatter',
+                'type': 'scatter',
+                'name': 'Replica -' + str(DELTA),
+                'marker': {
+                  'size': 8,
+                  'color': 'blue',
+                  'symbol': 'square',
+                },
               },
               {
                 'x': [
@@ -43,13 +85,30 @@ class QPSTimeseriesPlot(View):
             'layout': {
               'xaxis': {
                 'title': '[Date]',
-                'range': [ '2013-07-04 22:23:00', '2014-01-04 22:23:00' ]
+                'range': [ '2013-07-04 22:23:00', '2014-01-04 22:23:00' ],
+                'linecolor': 'black',
+                'mirror': True,
+                'ticks': 'outside',
+                'tickcolor': '#000',
+                'zeroline': False,
               },
               'yaxis': {
                 'title': '[mm]',
-                'range': [0, 8]
+                'range': [0 - DELTA, 8 + DELTA],
+                'linecolor': 'black',
+                'mirror': True,
+                'ticks': 'outside',
+                'tickcolor': '#000',
+                'zeroline': False,
               },
-              'title':'Data Labels Hover'
+              'title': {
+                "font": {
+                  "size": 20,
+                  "color": "blue",
+                  "family": "monospace",
+                },
+                'text': TITLE,
+              },
             },
             'config': {
               'displayModeBar': True,

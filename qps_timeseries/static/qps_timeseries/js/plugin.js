8157305ddb7b3a505e113501de36e767a682424e
Raruto <Raruto@users.noreply.github.com>
2023-12-12 12:38:40 +0100

    remove PluginService class

--- qps_timeseries/static/qps_timeseries/js/plugin.js
+++ qps_timeseries/static/qps_timeseries/js/plugin.js
@@ -1,8 +1,8 @@
 (function() {
 
-  const { Plugin, PluginService } = g3wsdk.core.plugin;
-  const { GUI }                   = g3wsdk.gui;
-  const { G3W_FID }               = g3wsdk.constant;
+  const { Plugin }  = g3wsdk.core.plugin;
+  const { GUI }     = g3wsdk.gui;
+  const { G3W_FID } = g3wsdk.constant;
 
   if (!globalThis.Plotly) {
     $script('https://cdn.plot.ly/plotly-1.52.2.min.js');
@@ -15,96 +15,43 @@
    */
   const DATE_REGEX = /^D\d{8}$/;
 
-  /**
-   * Chart component
-   */
-  const Chart = {
-    template: `
-      <section class="qps-timeseries">
-        <bar-loader :loading="loading"/>
-        <div ref="chart" style="margin-top: 10px;"></div>
-      </section>
-    `,
-
-    data() {
-      return {
-        loading: true, // show loading bar while getting data from server
-      }
-    },
-
-  };
-
-  class Service extends PluginService {
-
-    init(config = {}) {
-
-      /**
-       * @FIXME add description
-       */
-      this.config = config;
-
-      /**
-       * Store key and setter name method to eventually remove when plugin is removed
-       * 
-       * @type {{}}
-       */
-      this.keySetters = {};
-
-      /**
-       * Query result service
-       */
-      this.queryresultsService = GUI.getService('queryresults');
-
-      const keyOnAfteraddActionsForLayers = this.queryresultsService.onafter('addActionsForLayers', (actions, layers) => {
-        layers.forEach(layer => {
-          if (!layer.attributes.filter(attr => DATE_REGEX.test(attr.name)).length) {
-            return;
-          }
-          actions[layer.id].push({
-            id: 'qps-timeseries',
-            class: GUI.getFontClass('chart-line'),
-            hint: 'PS Time Series',
-            cbk: (layer, feature) => {
-              const fid   = feature.attributes[G3W_FID];
-              const chart = new (Vue.extend(Chart))();
-              GUI
-                .showModalDialog({
-                  message: chart.$mount().$el,
-                  size: 'large',
-                })
-                .on("shown.bs.modal", async function() {
-                  const { data, layout, config } = await (await fetch(initConfig.baseurl + 'qps_timeseries/api/some-protected-view/' + fid)).json();
-                  Plotly.newPlot(chart.$refs.chart, data, layout, config);
-                  chart.loading = false;
-                });
-            }
-          });
-        });
-      });
-
-      this.keySetters[keyOnAfteraddActionsForLayers] = 'addActionsForLayers';
-    }
-
-    /**
-     * Unlisten to setters events
-     */
-    clear() {
-      Object
-        .enties(this.keySetters)
-        .forEach(([key, setter]) => this.queryresultsService.un(setter, key));
-    }
-
-  }
-
   new (class extends Plugin {
     constructor() {
       super({
         name: 'qps_timeseries',
         i18n: {},
-        service: new Service,
       });
       if (this.registerPlugin(this.config.gid)) {
-        this.service.init(this.config);
+        GUI
+          .getService('queryresults')
+          .onafter('addActionsForLayers', (actions, layers) => {
+            layers.forEach(layer => {
+              if (!layer.attributes.some(attr => DATE_REGEX.test(attr.name))) {
+                return;
+              }
+              actions[layer.id].push({
+                id: 'qps-timeseries',
+                class: GUI.getFontClass('chart-line'),
+                hint: 'PS Time Series',
+                cbk: (layer, feature) => {
+                  const chart = new (Vue.extend({
+                    template: `<section class="qps-timeseries"><bar-loader :loading="loading"/><div ref="chart" style="margin-top: 10px;"></div></section>`,
+                    data:     () => ({ loading: true }), // show loading bar while getting data from server
+                  }))();
+                  GUI
+                    .showModalDialog({
+                      message: chart.$mount().$el,
+                      size: 'large',
+                    })
+                    .on("shown.bs.modal", async function() {
+                      const { data, layout, config } = await (await fetch(initConfig.baseurl + 'qps_timeseries/api/plot/' + layer.id + '/' + feature.attributes[G3W_FID])).json();
+                      Plotly.newPlot(chart.$refs.chart, data, layout, config);
+                      chart.loading = false;
+                    });
+                }
+              });
+            });
+          });
       }
       // hide loading icon on map
       this.setReady(true);
